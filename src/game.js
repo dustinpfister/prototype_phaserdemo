06f1b61ca4298790721cf19dff9d5361ca0b473c
dustinpfister <dustin.pfister@gmail.com>
2017-02-16 14:32:52 -0500

    pointers

--- src/game.js
+++ src/game.js
@@ -7,11 +7,20 @@ proto.create = function () {
 
     console.log('game state created');
 
+    app.input.addPointer()
+
     app.add.button(0, 0, 'button', function () {
 
         app.state.start('title');
 
     }, this, 0, 0, 1)
+
+};
+
+proto.render = function () {
+
+    app.debug.pointer(app.input.mousePointer);
+    app.debug.pointer(app.input.pointer1);
 
 };
 
